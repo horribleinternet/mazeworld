90c68746de04235c76d55b9c1e99f327766be0fb
horribleinternet <cyberblah@gmail.com>
2025-07-05 13:41:01 +0900

    draw a test pattern for the maze

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+from constants import *
 import pygame
+from viewport import Viewport
 
 def main():
-    print("main")
+    initialized = pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.
+    viewport = Viewport()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(pygame.Color(0,0,0))
+        viewport.draw(screen, None)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
--- viewport.py
+++ viewport.py
@@ -2,12 +2,13 @@ from constants import *
 from pygame import Color, draw;
 
 class Viewport:
-    def __init__(self, default_color = Color(192, 192, 192)):
+    def __init__(self, default_color = Color(192, 192, 192), default_width = 3):
         self.default_color = default_color
+        self.default_width = default_width
 
     def draw(self, surface, forward_map):
-        draw.lines(surface, self.default_color, True, [viewport_coord(0,0), viewport_coord(639, 0), viewport_coord(639, 479)])
-        draw.line(surface, self.default_color, viewport_coord(80, 0),  viewport_coord(320, 240))
-        draw.line(surface, self.default_color, viewport_coord(560, 0),  viewport_coord(320, 240))
-        draw.line(surface, self.default_color, viewport_coord(80, 479),  viewport_coord(320, 240))
-        draw.line(surface, self.default_color, viewport_coord(560, 479),  viewport_coord(320, 240))
+        draw.line(surface, self.default_color, viewport_coord(1., 0.), viewport_coord(1., 0.75), self.default_width)
+        draw.line(surface, self.default_color, viewport_coord(0.125, 0.0),  viewport_coord(0.5, 0.375), self.default_width)
+        draw.line(surface, self.default_color, viewport_coord(0.875, 0),  viewport_coord(0.5, 0.375), self.default_width)
+        draw.line(surface, self.default_color, viewport_coord(0.125, 0.75),  viewport_coord(0.5, 0.375), self.default_width)
+        draw.line(surface, self.default_color, viewport_coord(0.875, 0.75),  viewport_coord(0.5, 0.375), self.default_width)
