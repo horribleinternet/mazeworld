178a7eb447d4c01aebcdf8c0d99b529632e07bf6
horribleinternet <cyberblah@gmail.com>
2025-07-09 01:53:08 +0900

    try to finish a working map from a given viewpoint

--- direction.py
+++ direction.py
@@ -7,13 +7,16 @@ class Dir(Enum):
     SOUTH = 2
     WEST = 3
 
-DIR_VECTORS = { Dir.NORTH: (0, -1), Dir.EAST: (1, 0), Dir.SOUTH: (0, 1), Dir.WEST: (-1,0) }
+DIR_VECTORS = { Dir.NORTH: Vector2(0, -1), Dir.EAST: Vector2(1, 0), Dir.SOUTH: Vector2(0, 1), Dir.WEST: Vector2(-1,0) }
 
 DIR_REVERSE = { Dir.NORTH: Dir.SOUTH, Dir.EAST: Dir.WEST, Dir.SOUTH: Dir.NORTH, Dir.WEST: Dir.EAST }
 
 DIR_CLOCK = { Dir.NORTH: Dir.EAST, Dir.EAST: Dir.SOUTH, Dir.SOUTH: Dir.WEST, Dir.WEST: Dir.NORTH }
 
 DIR_COUNTER = { Dir.NORTH: Dir.WEST, Dir.EAST: Dir.NORTH, Dir.SOUTH: Dir.EAST, Dir.WEST: Dir.SOUTH }
+
+def dist_width(dist):
+    return ((dist + 1) * 2) + 1
 
 def dist_half_width(dist):
     return dist + 1
@@ -31,3 +34,20 @@ def seeable(facing, face, offset):
         if DIR_COUNTER[facing] == face:
             return True
     return False
+
+def vect_contained(vect, gridsize):
+    return vect.x >= 0 and vect.y >= 0 and vect.x < gridsize.x and vect.y < gridsize.y
+
+def get_line(start, facing, length, gridsize, skip_start = False):
+    line = []
+    pos = start.copy()
+    if skip_start:
+        pos += DIR_VECTORS[facing]
+        length -= 1
+    for i in range(length):
+        if not vect_contained(pos, gridsize):
+            line.append(None)
+        else:
+            line.append(pos)
+        pos += DIR_VECTORS[facing]
+    return line
--- map.py
+++ map.py
@@ -1,16 +1,42 @@
 from functools import reduce
 from block import get_default_block, Block
+from pygame import Vector2
+from direction import get_line, DIR_CLOCK, DIR_COUNTER, dist_width, dist_half_width
 
 class Map:
     def __init__(self, listgrid):
         self.listgrid = listgrid
         self.fill_in_map(listgrid)
         self.enclose_map(listgrid)
-        self.height = len(listgrid)
-        self.width = len(listgrid[0])
+        self.size = Vector2(len(listgrid[0]), len(listgrid))
+
+    def get_block(self, vect):
+        return self.listgrid[vect.y][vect.x]
 
     def get_viewmap(self, location, facing, distance = 3):
-        pass
+        viewmap = []
+        viewline = get_line(location, facing, distance, self.size)
+        #need to iterate in reversed order, but also track distance from start
+        for i in range(len(viewline)-1, -1, -1):
+            viewmap_row = []
+            if viewline[i] is None:
+                viewmap_row = [None for x in range(dist_width(i))]
+            else:
+                leftside = get_line(viewline[i], DIR_COUNTER[facing], dist_half_width(i), self.size, True)
+                rightside = get_line(viewline[i], DIR_CLOCK[facing], dist_half_width(i), self.size)
+                for place in reversed(leftside):
+                    if place is None:
+                        viewmap_row.append(None)
+                    else:
+                        viewmap_row.append(self.get_block(place))
+                for place in rightside:
+                    if place is None:
+                        viewmap_row.append(None)
+                    else:
+                        viewmap_row.append(self.get_block(place))
+            viewmap.append(viewmap_row)
+        return viewmap
+
 
     def fill_in_map(self, grid):
        max_width = reduce(max, map(len, grid))
@@ -25,3 +51,4 @@ class Map:
         for row in grid[1:-1]:
             row[0].solid = True
             row[-1].solid = True
+
