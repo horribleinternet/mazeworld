ba0e4d74e455be53a43a85ada87aef9f3f2b202c
horribleinternet <cyberblah@gmail.com>
2025-07-09 02:11:43 +0900

    draw box around the dungeon viewport

--- viewport.py
+++ viewport.py
@@ -8,6 +8,9 @@ class Viewport:
 
     def draw(self, forward_map):
         v = self.view
+        draw.line(v.surface, v.default_color, v.coordfunc(0., 0.), v.coordfunc(1., 0.), v.default_width)
+        draw.line(v.surface, v.default_color, v.coordfunc(0., 0.), v.coordfunc(0., 0.75), v.default_width)
+        draw.line(v.surface, v.default_color, v.coordfunc(0., 0.75), v.coordfunc(1., 0.75), v.default_width)
         draw.line(v.surface, v.default_color, v.coordfunc(1., 0.), v.coordfunc(1., 0.75), v.default_width)
         draw.line(v.surface, v.default_color, v.coordfunc(0.125, 0.),  v.coordfunc(0.5, 0.375), v.default_width)
         draw.line(v.surface, v.default_color, v.coordfunc(0.875, 0.),  v.coordfunc(0.5, 0.375), v.default_width)
