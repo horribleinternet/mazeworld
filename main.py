2f393c4b5f1885b51ccf0213ad2bcb93577e4e8f
horribleinternet <cyberblah@gmail.com>
2025-07-24 00:20:32 +0900

    add keystroke events to event loop

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import map
 from viewport import Viewport
 from view import View
 from direction import Dir, MapPos
+from player import Player
+from controller import Controller
 
 def main():
     initialized = pygame.init()
@@ -13,21 +15,27 @@ def main():
     dt = 0.
     view = View(screen, viewport_coord)
     viewport = Viewport(view)
+    testmap = map.Map(testmaps.testmap1)
+    player = Player()
+    controller = Controller(player, testmap)
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
+            match event.type:
+                case pygame.QUIT:
+                    return
+                case pygame.KEYDOWN:
+                    controller.input(event)
         screen.fill(pygame.Color(0,0,0))
         viewport.draw(None)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
 def test():
+    testmap = map.Map(testmaps.testmap1)
     map.printgrid(testmaps.testmap1)
     print("")
-    testmap = map.Map(testmaps.testmap1)
     map.printgrid(testmap.get_viewmap(MapPos(2,3), Dir.EAST, 4))
 
 if __name__ == "__main__":
-    #main()
-    test()
+    main()
+    #test()
