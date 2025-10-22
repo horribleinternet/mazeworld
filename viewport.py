abe768db134c53d11c729f4bf0dbb02155c0a93a
horribleinternet <cyberblah@gmail.com>
2025-10-22 17:26:37 +0900

    don't draw test lines

--- viewport.py
+++ viewport.py
@@ -9,12 +9,4 @@ class Viewport:
 
     def draw(self, forward_map):
         v = self.view
-        draw.line(v.surface, v.default_color, v.coordfunc(0., 0.), v.coordfunc(1., 0.), v.default_width)
-        draw.line(v.surface, v.default_color, v.coordfunc(0., 0.), v.coordfunc(0., 0.75), v.default_width)
-        draw.line(v.surface, v.default_color, v.coordfunc(0., 0.75), v.coordfunc(1., 0.75), v.default_width)
-        draw.line(v.surface, v.default_color, v.coordfunc(1., 0.), v.coordfunc(1., 0.75), v.default_width)
-        draw.line(v.surface, v.default_color, v.coordfunc(0.125, 0.),  v.coordfunc(0.5, 0.375), v.default_width)
-        draw.line(v.surface, v.default_color, v.coordfunc(0.875, 0.),  v.coordfunc(0.5, 0.375), v.default_width)
-        draw.line(v.surface, v.default_color, v.coordfunc(0.125, 0.75),  v.coordfunc(0.5, 0.375), v.default_width)
-        draw.line(v.surface, v.default_color, v.coordfunc(0.875, 0.75),  v.coordfunc(0.5, 0.375), v.default_width)
         printgrid(forward_map)
