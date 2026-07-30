80614b874e50fc46132d349081c60028dd16693a
horribleinternet <cyberblah@gmail.com>
2026-07-30 16:15:47 +0900

    qq

--- world.py
+++ world.py
@@ -10,4 +10,3 @@ class World:
         viewport.draw(self.map.get_viewmap(self.controller.player.pos, self.controller.player.facing), self.controller.player.facing)
         pygame.display.flip()
 
-
